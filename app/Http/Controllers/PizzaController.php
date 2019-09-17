3b25b6aa60791be1a7530f5ba9be91a63beb2002
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-09-17 14:02:46 +0530

    Serve pizzas

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Pizza;
 
 class PizzaController extends Controller
 {
-    
+    public function index() {
+      return Pizza::all();
+    }
 }
